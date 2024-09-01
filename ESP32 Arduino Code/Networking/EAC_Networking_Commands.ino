a1f4641953d1ec76a92b2a5a174a1e63eb6b8b89
Fida Khandaker Safa <safa2015.fk@gmail.com>
2024-09-01 20:12:12 +1000

    EAC_Networking

--- ESP32 Arduino Code/Networking/EAC_Networking_Commands.ino
+++ ESP32 Arduino Code/Networking/EAC_Networking_Commands.ino
@@ -1,29 +1,86 @@
 
-void ccpForward() {
+class EAC_Networking_Commands
+{
 
-}
+    void ccpForward()
+    {
+        Serial.println("Moving forward...");
+        speed = 32;
+        String jsonString;
+        if (serialiseJSON(jsonString, "CCP", "Forward", speed, "Closed", "On"))
+        {
+            // Send jsonString over the network
+            sendData(jsonString);
+        }
+    }
 
+    void ccpBackward()
+    {
+        Serial.println("Moving backward...");
+        speed = -22;
+        String jsonString;
+        if (serialiseJSON(jsonString, "CCP", "Backward", speed, "Closed", "On"))
+        {
+            // Send jsonString over the network
+            sendData(jsonString);
+        }
+    }
 
-void ccpBackward() {
+    void ccpStop()
+    {
+        Serial.println("Stopping...");
+        speed = 0;
+        String jsonString;
+        if (serialiseJSON(jsonString, "CCP", "Stop", speed, "Closed", "On"))
+        {
+            // Send jsonString over the network
+            sendData(jsonString);
+        }
+    }
 
-}
+    void ccpEmergencyStop()
+    {
+        Serial.println("Emergency Stop!");
+        speed = 0;
+        String jsonString;
+        if (serialiseJSON(jsonString, "CCP", "Emergency_Stop", speed, "Closed", "Off"))
+        {
+            // Send jsonString over the network
+            sendData(jsonString);
+        }
+    }
 
-void ccpStop() {
+    void ccpOpenDoor()
+    {
+        Serial.println("Opening door...");
+        String jsonString;
+        if (serialiseJSON(jsonString, "CCP", "Open_Door", 0, "Open", "On"))
+        {
+            // Send jsonString over the network
+            sendData(jsonString);
+        }
+    }
 
-}
+    void ccpCloseDoor()
+    {
+        Serial.println("Closing door...");
+        String jsonString;
+        if (serialiseJSON(jsonString, "CCP", "Close_Door", 0, "Closed", "On"))
+        {
+            // Send jsonString over the network
+            sendData(jsonString);
+        }
+    }
 
-void ccpEmergencyStop() {
-
-}
-
-void ccpOpenDoor() {
-
-}
-
-void ccpCloseDoor() {
-
-}
-
-void eacStat() {
-
+    void eacStat()
+    {
+        Serial.println("Retrieving EAC status...");
+        // This function might involve collecting current status data from EAC
+        String jsonString;
+        if (serialiseJSON(jsonString, "EAC", "Status", speed, "Closed", "On"))
+        {
+            // Send jsonString over the network
+            sendData(jsonString);
+        }
+    }
 }
--- ESP32 Arduino Code/Networking/EAC_Networking_Control.ino
+++ ESP32 Arduino Code/Networking/EAC_Networking_Control.ino
@@ -1,25 +1,132 @@
+#include <WiFiClient.h>
+#include <ArduinoJson.h>
+class EAC_Networking_Control
+{
+private:
+    String ipAddress;
+    int ccpPort;
+    WiFiClient client;
+    File receivedJSON;
+    File JSONToSend;
 
+public:
+    // Constructor
+    EAC_Networking_Control(String ip, int port) : ipAddress(ip), ccpPort(port)
+    {
+        ipAddress = "10.20.30.109";
+        ccpPort = 3009;
+    }
 
-void setup() {
+    void setup()
+    {
+        Serial.begin(9600);
+        if (WiFi.begin() != WL_CONNECTED)
+        {
+            Serial.println("Connecting to WiFi...");
+            while (WiFi.status() != WL_CONNECTED)
+            {
+                delay(1000);
+                Serial.print(".");
+            }
+            Serial.println("Connected to WiFi");
+        }
+    }
 
-}
+    void loop()
+    {
+        if (client.connected())
+        {
+            sendData();
+            receiveJSON();
+        }
+    }
 
-void loop() {
+    void connectToCCP()
+    {
+        if (!client.connect(ipAddress.c_str(), ccpPort))
+        {
+            Serial.println("Connection to CCP failed");
+        }
+        else
+        {
+            Serial.println("Connected to CCP");
+        }
+    }
 
-}
+    void sendData()
+    {
+        if (client.connected())
+        {
+            String jsonString;
+            if (serialiseJSON(jsonString))
+            {
+                client.print(jsonString);
+                Serial.println("Data sent: " + jsonString);
+            }
+            else
+            {
+                Serial.println("Failed to serialize JSON");
+            }
+        }
+        else
+        {
+            Serial.println("Client not connected, cannot send data");
+        }
+    }
 
-void connectToCCP() {
-    
-}
+    // void createJSON()
+    // {
+    // }
+    bool serialiseJSON(String &jsonString, String clientType, String message, int speed, String dStatus, String mStatus)
+    {
+        JsonDocument doc;
 
-void sendData() {
+        doc["client_type"] = clientType;
+        doc["message"] = message;
+        doc["speed"] = speed;
+        doc["dStatus"] = dStatus;
+        doc["mStatus"] = mStatus;
 
-}
+        if (serializeJson(doc, jsonString) == 0)
+        {
+            return false; // Serialization failed
+        }
+        return true; // Successfully serialized
+    }
 
-void createJSON() {
+    void deserialiseJSON(String jsonString)
+    {
+        JsonDocument doc;
+        DeserializationError error = deserializeJson(doc, jsonString);
 
-}
+        if (error)
+        {
+            Serial.print(F("Failed to read JSON: "));
+            Serial.println(error.f_str());
+            return;
+        }
 
-void receiveJSON() {
-    
+        const char *clientType = doc["client_type"];
+        const char *message = doc["message"];
+        int speed = doc["speed"];
+        const char *dStatus = doc["dStatus"];
+        const char *mStatus = doc["mStatus"];
+
+        // Print the received values unnecessary
+        Serial.println("Received JSON:");
+        Serial.println("client_type: " + String(clientType));
+        Serial.println("message: " + String(message));
+        Serial.println("speed: " + String(speed));
+        Serial.println("dStatus: " + String(dStatus));
+        Serial.println("mStatus: " + String(mStatus));
+    }
+
+    void receiveJSON()
+    {
+        if (client.available())
+        {
+            String jsonString = client.readStringUntil('\n');
+            deserialiseJSON(jsonString);
+        }
+    }
 }
