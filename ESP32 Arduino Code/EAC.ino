3fb703c903c19d3f53d4d9212ec21ff7adc016f2
Fida Khandaker Safa <safa2015.fk@gmail.com>
2024-09-01 21:08:26 +1000

    SetMotor update

--- ESP32 Arduino Code/EAC.ino
+++ ESP32 Arduino Code/EAC.ino
@@ -6,29 +6,35 @@ Servo sgServo;
 int MOTOR_PIN = 5;
 int SERVO_PIN = 6;
 
-void setup() {
-
+void setup()
+{
 }
 
-void loop() {
-    
-    while(mStatus = 1) {
+void loop()
+{
+
+    while (mStatus = 1)
+    {
         setMotor(speed);
         serial.delay(200);
     }
 
-    if(dStatus = 0) {
+    if (dStatus = 0)
+    {
         setServo(0);
-    } else {
+    }
+    else
+    {
         setServo(180);
     }
-    
 }
 
-void setMotor(int) {
-    
+void setMotor(int x)
+{
+    x = map(speed, 0, 255, 0, 251);
+    dcMotor.write(x);
 }
 
-void setServo(int) {
-
+void setServo(int)
+{
 }
