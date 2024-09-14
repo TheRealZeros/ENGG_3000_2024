48847a1585e452a88c12a5fa3b82b40e29229e78
Gabriel D'Costa <65882215+Gabriel-dcosta@users.noreply.github.com>
2024-09-14 20:58:12 +1000

    Prettified EAC
    Added comments & added functions

--- ESP32 Arduino Code/EAC.ino
+++ ESP32 Arduino Code/EAC.ino
@@ -1,3 +1,7 @@
+// Last update: 2024-09-14
+// BR09 EAC Code: Comms_4
+
+// Libraries
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WiFiUdp.h>
@@ -6,6 +10,7 @@
 #include <stdlib.h>
 #include <ESP32Servo.h>
 
+// PIN Definitions
 #define CLK_DIR 17
 #define EN_DIR 14
 #define SER_DIR 12
@@ -17,26 +22,108 @@
 #define M1_PWM2A 23
 #define Latch_DIR 19
 
-Servo myServo1;
-Servo myServo2;
+// Component Variables
+Servo doorServoL;
+Servo doorServoR;
 
-
+// Network Variables
 WiFiUDP udp;
-JsonDocument json;
-static float acc = 10; // mm/s2
-float targetSpeed = 180;
-double speed = 0;
-unsigned long oldTime = 0;
-unsigned long currentTime = 0;
-unsigned long deltaTime = 0;
 char readBuffer[255];
-
-//IPAddress host(127,0,0,1);
-
 char ssid[] = "ENGG2K3K";
 char pass[] = "";
 int udpPort = 3009;
 char* udpAdd = "10.20.30.176";
+JsonDocument json;
+
+// EAC Variables
+static float acc = 10; // mm/s2
+float targetSpeed = 180;
+double currentSpeed = 0;
+
+// Time Variables
+unsigned long oldTime = 0;
+unsigned long currentTime = 0;
+unsigned long deltaTime = 0;
+
+///////////////////////////////////////// Intial ESP32 Setup /////////////////////////////////////////
+void setup() {
+  
+  startWifi();
+  startUDP();
+  setupMotors();
+  setupServos();
+  Serial.println("Setup has finished");
+
+}
+
+///////////////////////////////////////// ESP32 Loop /////////////////////////////////////////
+void loop() {
+  // Get the most approximate time from micros()  
+  getDeltaTime();
+
+  // Read the JSON message from the CCP
+  readJson();
+
+  // Spin the motor & update depending on variables
+  spinMotor();
+  updateMotors();
+  updateDoors();
+
+  // Delay for 100ms
+  delay(100);
+}
+
+///////////////////////////////////////// Functions /////////////////////////////////////////
+
+void getDeltaTime() {
+  currentTime = micros();
+  deltaTime = currentTime - oldTime;
+  oldTime = currentTime;
+}
+
+void startWifi() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, pass);
+  Serial.println("Wifi has started");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void startUDP() {
+  udp.begin(udpPort);
+  Serial.println(udpPort);
+  Serial.println("UDP has started");
+  firstUDPmsg();
+}
+
+void firstUDPmsg() {
+  const uint8_t buffer[50] = "hello world!";
+  udp.beginPacket(udpAdd, udpPort);
+  udp.write(buffer, 11);
+  udp.endPacket();
+  Serial.println("udp mesg sent");
+}
+
+void setupMotors() {
+  pinMode(MotorPinA, OUTPUT);
+  pinMode(MotorPinB, OUTPUT);
+  pinMode(MotorPinP, OUTPUT);
+}
+
+void spinMotor() {
+  digitalWrite(MotorPinA, HIGH);
+  digitalWrite(MotorPinB, LOW);
+}
+
+void setupServos() {
+  doorServoL.setPeriodHertz(50); // PWM frequency for SG90
+  doorServoR.setPeriodHertz(50); 
+  doorServoL.attach(Servo1_PWM1A, 500, 2400); // Minimum and maximum pulse width (in µs) 
+  doorServoR.attach(Servo2_PWM1B, 500, 2400); // Minimum and maximum pulse width (in µs) 
+}
 
 void readJson() {
   int packetSize = udp.parsePacket();
@@ -44,7 +131,7 @@ void readJson() {
     int size = udp.read(readBuffer, packetSize);
     //readBuffer[size] = '\0';
     Serial.println(readBuffer);
-    Serial.println(speed);
+    Serial.println(curentSpeed);
     deserializeJson(json, readBuffer);
 
     const uint8_t buffer[50] = "recieved!";
@@ -61,79 +148,18 @@ void updateDoors() {
 
 void updateMotors() {
   targetSpeed = json["speed"].as<float>();
-  if(speed != targetSpeed) {
+  if(currentSpeed != targetSpeed) {
     double accDelta = (acc*deltaTime)/1000;
-    if(abs(speed - targetSpeed) < accDelta) {
-      speed = targetSpeed;
-    } else if(speed > targetSpeed) {
-      speed = speed - accDelta;
+    if(abs(currentSpeed - targetSpeed) < accDelta) {
+      currentSpeed = targetSpeed;
+    } else if(currentSpeed > targetSpeed) {
+      currentSpeed = currentSpeed - accDelta;
     } else {
-      speed = speed + accDelta;
+      currentSpeed = currentSpeed + accDelta;
     }
   }
   if(targetSpeed = 10) {
-    speed = 100;
+    currentSpeed = 100;
   }
-  digitalWrite(MotorPinP, speed);
-}
-
-void setup() {
-
-
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  WiFi.begin(ssid, pass);
-  Serial.println("wifi began");
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(100);
-    Serial.print(".");
-  }
-  Serial.println(WiFi.localIP());
-  udp.begin(udpPort);
-  Serial.println(udpPort);
-  Serial.println("udp began");
-
-  pinMode(MotorPinA, OUTPUT);
-  pinMode(MotorPinB, OUTPUT);
-  pinMode(MotorPinP, OUTPUT);
-  // digitalWrite(MotorPinA, HIGH);
-  // digitalWrite(MotorPinB, LOW);
-  myServo1.setPeriodHertz(50); // PWM frequency for SG90
-  myServo2.setPeriodHertz(50); 
-  myServo1.attach(Servo1_PWM1A, 500, 2400); // Minimum and maximum pulse width (in µs) 
-  myServo2.attach(Servo2_PWM1B, 500, 2400); // Minimum and maximum pulse width (in µs) 
-
-  const uint8_t buffer[50] = "hello world!";
-  udp.beginPacket(udpAdd, udpPort);
-  udp.write(buffer, 11);
-  udp.endPacket();
-  Serial.println("udp mesg sent");
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  currentTime = micros();
-  deltaTime = currentTime - oldTime;
-  oldTime = currentTime;
-
-  readJson();
-  digitalWrite(MotorPinA, HIGH);
-  digitalWrite(MotorPinB, LOW);
-  updateMotors();
-  updateDoors();
-  //Serial.printf(" millis: '%d'; delta: %d; accDelta: %f", currentTime, deltaTime, accDelta);
-  //myServo1.write(speed);
-
-//    //rotation from 0 to 180°
-//   for (int pos = 0; pos <= 180; pos += 1) {
-//     myServo1.write(pos);
-//     myServo2.write(pos);
-//     delay(10);
-//   }
-//  // Rotation from 180° to 0
-//   for (int pos = 180; pos >= 0; pos -= 1) {
-//     myServo1.write(pos);
-//     myServo2.write(pos);
-//     delay(10);
-//   }
+  digitalWrite(MotorPinP, currentSpeed);
 }
